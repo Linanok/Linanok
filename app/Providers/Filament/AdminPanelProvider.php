edd3c2e2120d245bc4ee238447a4d4d488e59d49
Siavash Bamshadnia <sbamtr@gmail.com>
2025-07-13 18:01:38 +0330

    feat: add password reset functionality to AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,6 +44,7 @@ class AdminPanelProvider extends PanelProvider
             })
             ->login()
             ->profile(isSimple: false)
+            ->passwordReset()
             ->sidebarCollapsibleOnDesktop()
             ->spa()
             ->unsavedChangesAlerts()
